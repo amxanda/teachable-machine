d7aad78ddd6bdee16429f1d04a9dfc423ce5a3bb
Amanda <91753947+amxanda@users.noreply.github.com>
2024-04-01 04:41:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,59 +20,77 @@ async function createModel() {
     return recognizer;
 }
 
+var alarm = 0;
+var baby = 0;
 var noise = 0;
 var dog = 0;
-var baby = 0;
-var alarm = 0;
 
 async function init() {
     const recognizer = await createModel();
     const classLabels = recognizer.wordLabels(); // get class labels
     const labelContainer = document.getElementById("label-container");
+
     for (let i = 0; i < classLabels.length; i++) {
         labelContainer.appendChild(document.createElement("div"));
     }
+
+    const alertMessage = document.createElement('div');
+    alertMessage.style.color = 'white';
+    alertMessage.style.fontSize = '36px';
+    alertMessage.style.fontWeight = 'bold';
+    alertMessage.style.alignContent = 'center';
+    alertMessage.style.textAlign = 'center';
+    alertMessage.style.marginTop = '100px';
+    alertMessage.style.backgroundColor = 'red';
+    alertMessage.style.borderRadius = '10px';
+    labelContainer.appendChild(alertMessage);
 
     // listen() takes two arguments:
     // 1. A callback function that is invoked anytime a word is recognized.
     // 2. A configuration object with adjustable fields
     recognizer.listen(result => {
         const scores = result.scores; // probability of prediction for each class
-        const labelImage = document.getElementById('label-image')
+        const labelImage = document.getElementById('label-image');
         // render the probability scores per class
+
+        alertMessage.innerHTML = '';
 
         for (let i = 0; i < classLabels.length; i++) {
             if (result.scores[i].toFixed(2) >= 0.85) {
-                if(i === 0){
+                if (i === 0) /*Alarm*/ {
                     labelImage.innerHTML = "";
                     document.body.style.backgroundImage = "url('img/alarm.jpg')";
                     document.body.style.backgroundSize = 'cover';
                     document.body.style.backgroundPosition = 'center';
+
+                    // Aviso na tela
+                    alertMessage.innerHTML = 'Alarme detectado!'; 
                 }
-                if(i === 1){
+                if (i === 1) /*Baby*/ {
                     labelImage.innerHTML = "";
                     document.body.style.backgroundImage = "url('img/baby.jpg')";
                     document.body.style.backgroundSize = 'cover';
                     document.body.style.backgroundPosition = 'center';
                 }
-                if(i === 2){
+                if (i === 2) /*Background Noise*/ {
                     labelImage.innerHTML = "";
                     document.body.style.backgroundImage = "url('img/noise.jpeg')";
                     document.body.style.backgroundSize = 'cover';
                     document.body.style.backgroundPosition = 'center';
                 }
-                if(i === 3){
+                if (i === 3) /*Dog*/ {
                     labelImage.innerHTML = "";
                     document.body.style.backgroundImage = "url('img/dog.avif')";
                     document.body.style.backgroundSize = 'cover';
                     document.body.style.backgroundPosition = 'center';
                 }
-            } else{
+            } else {
                 labelContainer.childNodes[i].innerHTML = null;
             }
 
             const classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
+            labelContainer.childNodes[i].style.color = '#FFF';
         }
     }, {
         includeSpectrogram: true, // in case listen should return result.spectrogram
@@ -80,21 +98,4 @@ async function init() {
         invokeCallbackOnNoiseAndUnknown: true,
         overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
     });
-
-    // Stop the recognition in 5 seconds.
-    // setTimeout(() => recognizer.stopListening(), 5000);
 }
-
-// async function image() {
-    
-
-//     if (noise > baby && noise > alarm && noise > dog) {
-        
-//     } else if (alarm > baby && alarm > dog && alarm > noise) {
-        
-//     } else if (baby > alarm && baby > dog && baby > noise) {
-        
-//     } else if (dog > alarm && dog > noise && dog > baby) {
-        
-//     }
-// }
